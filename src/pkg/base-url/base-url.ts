8dd303412a4ad2cd26694bbba5d6e5809168477d
Daniela Chokan <d.chokan@rubylabs.com>
2026-07-01 19:25:19 +0300

    refactor: remove conflicting code for VERCEL_URL handling in getApiBaseUrl function

--- src/pkg/base-url/base-url.ts
+++ src/pkg/base-url/base-url.ts
@@ -2,9 +2,5 @@ import { envClient } from '@/config/env'
 
 export const getApiBaseUrl = (): string => {
   if (typeof window !== 'undefined') return ''
-<<<<<<< HEAD
-=======
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
->>>>>>> d3054faddd259ab894949e7ada1a8f29a8e74a5a
   return envClient.NEXT_PUBLIC_APP_URL
 }
